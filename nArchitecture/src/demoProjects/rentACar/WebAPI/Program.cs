d7f3284648fe7b77dacaffae24ed3323165cdeee
Ayşe Akışık <ayseakisik16@gmail.com>
2022-10-22 23:37:39 +0300

    brand operations completed

--- nArchitecture/src/demoProjects/rentACar/WebAPI/Program.cs
+++ nArchitecture/src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices();
 //builder.Services.AddSecurityServices();
 builder.Services.AddPersistenceServices(builder.Configuration);
 //builder.Services.AddInfrastructureServices();
@@ -23,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
